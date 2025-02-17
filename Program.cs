af0631abc1c6d07344d2e550ae07767664ae4bbe
Chelik2 <aldarmalanov7@gmail.com>
2025-02-17 16:06:37 +0800

    Fix code formatting

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");   // Добавьте пробел или измените отступ
+            Console.WriteLine("Hello, World!");
+            
         }
     }
